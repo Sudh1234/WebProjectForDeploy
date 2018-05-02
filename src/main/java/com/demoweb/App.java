953dbc29d4972086bfffc5dbf5df2c340a89d837
skumar <skumar@securview.com>
2018-05-02 16:08:03 +0530

    remove unwanted code
    Change-Id: Ie22d195ca2773b9bde68ba4e967032a780cc22fb

--- src/main/java/com/demoweb/App.java
+++ src/main/java/com/demoweb/App.java
@@ -2,22 +2,7 @@ package com.demoweb;
 
 public class App 
 {
-	public static void main( String[] args )
-	{
-		printMessage();
-	}
-
-	public static String getMessage() {
-
-		//feat-02
-		String str = "Feature 02";
-		return "Hello reviewer! This is : " + str;
-	}
-
-	public static void printMessage() {
-		System.out.println(getMessage());
-	}
-
+	
 	public boolean isEvenNumber(int number){
 
 		boolean result = false;
