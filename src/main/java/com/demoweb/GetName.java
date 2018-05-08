973172623b62b607ec5cfaaee67181672674ddcd
skumar <skumar@securview.com>
2018-05-08 11:28:09 +0530

    Quality gate test
    Change-Id: Ib7271e4a261f8cd714c444e6ce8dcc575d54f89f

--- src/main/java/com/demoweb/GetName.java
+++ src/main/java/com/demoweb/GetName.java
@@ -1,16 +1,10 @@
 package com.demoweb;
 
 public class GetName {
-	public static String s;
-	
-	public static String s3;
 
-	public void Getname() {
+	public void getname() {
 		
-		s = "ew";
-		s3 ="w";
-		System.out.println("d "+s);
-		
+		int o =0;
 		
 	}
 	
